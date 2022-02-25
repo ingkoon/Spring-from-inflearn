426d05502c0f599da08a553fbb0f4e2e2f3be855
ingkoon <ing97220@naver.com>
2022-02-25 12:53:56 +0900

    [2022-02-25] xml config

--- core/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ core/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -4,7 +4,7 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 
 public class FixDiscountPolicy implements DiscountPolicy {
-
+//11111
     private int discountFixAmount = 1000;
 
     @Override
