3f6e0f6ffc9fceee147cdaffc25f8f50e81c3078
ingkoon <ing97220@naver.com>
2022-03-03 15:25:18 +0900

    [2022-03-02] lifecycle(2)

--- core/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,7 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient  implements InitializingBean, DisposableBean {
+public class NetworkClient  {
     private String url;
 
     public NetworkClient() {
@@ -29,16 +27,15 @@ public class NetworkClient  implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지 ");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+
+    public void close(){
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
